6add55d65dc33b0621134ad07aa57aa46d2a92a7
Kiran Kumar <kirankumarrajoju@hcl.com>
2021-02-03 08:18:23 +0000

    Added Source file and tests

--- models/stages/jaffle_shop/stg_customers.sql
+++ models/stages/jaffle_shop/stg_customers.sql
@@ -1,12 +1,6 @@
-with customers as (
-
     select
         id as customer_id,
         first_name,
         last_name
 
-    from jaffle_shop.customers
-
-)
-
-select * from customers
+    from {{ source('jaffle_shop','customers') }}
