e472c813740a26fba52136e3e617cdd14fb2a80f
Kiran Kumar <kirankumarrajoju@hcl.com>
2021-02-10 14:12:35 +0000

    updated

--- snapshots/mock_orders_snapshot_hard_delete.sql
+++ snapshots/mock_orders_snapshot_hard_delete.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {% snapshot mock_orders_snapshot_hard_delete %}
 
     {{
@@ -13,20 +12,4 @@
 
     select * from {{ source('jaffle_shop', 'mock_orders') }}
 
-=======
-{% snapshot mock_orders_snapshot_hard_delete %}
-
-    {{
-        config(
-          target_schema='jaffle_shop',
-          strategy='timestamp',
-          unique_key='order_id',
-          updated_at='updated_at',
-          invalidate_hard_deletes=True,
-        )
-    }}
-
-    select * from {{ source('jaffle_shop', 'mock_orders') }}
-
->>>>>>> 532845fe153e9b54767f9404c3ec6dc2d12e441b
 {% endsnapshot %}
