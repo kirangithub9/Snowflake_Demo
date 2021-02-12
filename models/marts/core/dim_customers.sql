90455f5d2ebd6da800e646ba311d6453c7cdc0e0
Kiran Kumar <kirankumarrajoju@hcl.com>
2021-02-12 16:31:35 +0000

    added employee seed with the customer cte

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -4,13 +4,16 @@ with customers as (
 orders as (
     select * from {{ ref('fct_orders')}}
 ),
+employee as (
+    select * from {{ ref('employee')}}
+),
 customer_orders as (
     select
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        --sum(amount) as lifetime_value
+        sum(amount) as lifetime_value
     from orders
     group by 1
 ),
@@ -19,11 +22,15 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employee.email,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
     from customers
+    join employee using (customer_id)
     left join customer_orders using (customer_id)
 )
+
+    
 select * from final
